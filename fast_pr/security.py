a29de93f3f847e35715a91a661b78df7291a4494
Neima <a01399122@gmail.com>
2026-03-28 23:17:05 -0300

    token creation

---
+++ fast_pr/security.py
@@ -0,0 +1,17 @@
+from datetime import datetime,timedelta
+from zoneinfo import ZoneInfo
+
+from jwt import encode
+
+SECRET_KEY = ""
+ALGORITHM = "HS256"
+ACESSS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_acess_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.now(tz=ZoneInfo('UTX')) + timedelta(ACESSS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({'exp':expire})
+    encoded_jwt = encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+
+    return encoded_jwt
